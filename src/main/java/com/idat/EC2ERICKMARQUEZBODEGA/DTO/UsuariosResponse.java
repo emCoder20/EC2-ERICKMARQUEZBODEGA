7e3c552742b7dc97e706f40e70d8e23de19542eb
emCoder20 <100981739+emCoder20@users.noreply.github.com>
2022-06-29 00:12:30 -0500

    EC2

--- src/main/java/com/idat/EC2ERICKMARQUEZBODEGA/DTO/UsuariosResponse.java
+++ src/main/java/com/idat/EC2ERICKMARQUEZBODEGA/DTO/UsuariosResponse.java
@@ -2,6 +2,43 @@ package com.idat.EC2ERICKMARQUEZBODEGA.DTO;
 
 public class UsuariosResponse {
 
+    private Integer idUsuario;
+    private String usuario;
+    private String password;
+    private String rol;
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
     private String token;
 
     public String getToken() {
